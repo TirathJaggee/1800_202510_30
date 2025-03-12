1e853b9dc23f79972609f2471cc629747a36d78b
Tirath <tjaggee@my.bcit.ca>
2025-03-12 02:37:54 -0700

    adding questions to game

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -11,6 +11,72 @@ function loadScreen() {
     });
 }
 
+questionsRef.add({
+    answer: 2,
+    category: "carbs",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/pancakes.jpg",
+    image2: "./images/questions/oatmeal.jpg"
+});
+questionsRef.add({
+    answer: 2,
+    category: "protien",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/salmon.jpg",
+    image2: "./images/questions/fried_chicken.jpg"
+});
+questionsRef.add({
+    answer: 2,
+    category: "protien",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/steak.jpg",
+    image2: "./images/questions/tofu.jpg"
+});
+questionsRef.add({
+    answer: 2,
+    category: "carbs",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/pasta.jpg",
+    image2: "./images/questions/quinoa.jpg"
+});
+questionsRef.add({
+    answer: 1,
+    category: "fats",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/black_coffee.jpg",
+    image2: "./images/questions/latte.jpg"
+});
+questionsRef.add({
+    answer: 1,
+    category: "carbs",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/whole_wheat_bread.jpg",
+    image2: "./images/questions/white_bread.jpg"
+});
+questionsRef.add({
+    answer: 1,
+    category: "vitamins",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/strawberries.jpg",
+    image2: "./images/questions/banana.jpg"
+});
+questionsRef.add({
+    answer: 1,
+    category: "protien",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/beef.jpg",
+    image2: "./images/questions/fries.jpg"
+});
+questionsRef.add({
+    answer: 2,
+    category: "carbs",
+    description: "Which has fewer calories?",
+    image1: "./images/questions/pretzels.jpg",
+    image2: "./images/questions/nachos.jpg"
+});
+
+
+
 function checkAnswer(val) {
     console.log(val);
 }
