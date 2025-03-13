f69c608ccc0aa85380d70a6f674d641629393845
Tirath <tjaggee@my.bcit.ca>
2025-03-12 17:51:36 -0700

    fixing question database

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,5 +1,5 @@
 function loadScreen() {
-    $('#game_placeholder').load('/html/game_stage1.html', function() {
+    $('#game_placeholder').load('/html/game_stage1.html', function () {
         // This callback runs after the content is loaded
         // Now it's safe to attach event listeners to elements in the loaded content
         document.getElementById("firstImage").addEventListener("click", () => {
@@ -10,106 +10,107 @@ function loadScreen() {
         });
     });
 }
+function addQuestions() {
+    questionsRef.add({
+        answer: 2,
+        category: "carbs",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/pancakes.jpg",
+        image2: "./images/questions/oatmeal.jpg"
+    });
+    questionsRef.add({
+        answer: 2,
+        category: "protien",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/salmon.jpg",
+        image2: "./images/questions/fried_chicken.jpg"
+    });
+    questionsRef.add({
+        answer: 2,
+        category: "protien",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/steak.jpg",
+        image2: "./images/questions/tofu.jpg"
+    });
+    questionsRef.add({
+        answer: 2,
+        category: "carbs",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/pasta.jpg",
+        image2: "./images/questions/quinoa.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "fats",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/black_coffee.jpg",
+        image2: "./images/questions/latte.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "carbs",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/whole_wheat_bread.jpg",
+        image2: "./images/questions/white_bread.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "vitamins",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/strawberries.jpg",
+        image2: "./images/questions/banana.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "protien",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/beef.jpeg",
+        image2: "./images/questions/fries.jpg"
+    });
+    questionsRef.add({
+        answer: 2,
+        category: "carbs",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/pretzels.jpg",
+        image2: "./images/questions/nachos.jpg"
+    });
+    questionsRef.add({
+        answer: 2,
+        category: "fats",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/whole_milk.jpg",
+        image2: "./images/questions/skim_milk.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "vitamins",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/watermelon.jpg",
+        image2: "./images/questions/mango.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "protien",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/grilled_chicken_sandwich.jpg",
+        image2: "./images/questions/cheeseburger.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "fats",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/popcorn.jpg",
+        image2: "./images/questions/potato_chips.jpg"
+    });
+    questionsRef.add({
+        answer: 1,
+        category: "protien",
+        description: "Which has fewer calories?",
+        image1: "./images/questions/shrimp.jpg",
+        image2: "./images/questions/fried_fish.jpg"
+    });
 
-questionsRef.add({
-    answer: 2,
-    category: "carbs",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/pancakes.jpg",
-    image2: "./images/questions/oatmeal.jpg"
-});
-questionsRef.add({
-    answer: 2,
-    category: "protien",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/salmon.jpg",
-    image2: "./images/questions/fried_chicken.jpg"
-});
-questionsRef.add({
-    answer: 2,
-    category: "protien",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/steak.jpg",
-    image2: "./images/questions/tofu.jpg"
-});
-questionsRef.add({
-    answer: 2,
-    category: "carbs",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/pasta.jpg",
-    image2: "./images/questions/quinoa.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "fats",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/black_coffee.jpg",
-    image2: "./images/questions/latte.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "carbs",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/whole_wheat_bread.jpg",
-    image2: "./images/questions/white_bread.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "vitamins",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/strawberries.jpg",
-    image2: "./images/questions/banana.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "protien",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/beef.jpg",
-    image2: "./images/questions/fries.jpg"
-});
-questionsRef.add({
-    answer: 2,
-    category: "carbs",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/pretzels.jpg",
-    image2: "./images/questions/nachos.jpg"
-});
-questionsRef.add({
-    answer: 2,
-    category: "fats",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/whole_milk.jpg",
-    image2: "./images/questions/skim_milk.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "vitamins",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/watermelon.jpg",
-    image2: "./images/questions/mango.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "protien",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/grilled_chicken_sandwich.jpg",
-    image2: "./images/questions/cheeseburger.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "fats",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/popcorn.jpg",
-    image2: "./images/questions/potato_chips.jpg"
-});
-questionsRef.add({
-    answer: 1,
-    category: "protien",
-    description: "Which has fewer calories?",
-    image1: "./images/questions/shrimp.jpg",
-    image2: "./images/questions/fried_fish.jpg"
-});
-
+}
 
 
 
@@ -118,6 +119,6 @@ function checkAnswer(val) {
 }
 
 // A $( document ).ready() block.
-$(document).ready(function() {
+$(document).ready(function () {
     loadScreen(); // Move the function call here to ensure DOM is ready
 });
